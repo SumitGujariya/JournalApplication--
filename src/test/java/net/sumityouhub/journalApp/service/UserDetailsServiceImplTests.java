5d5264a93b022d7ba6c0aaac3077077e7d12f756
Sumit Gujariya <gujariyasumit96@gmail.com>
2026-03-12 15:34:47 +0530

    Test cases

---
+++ src/test/java/net/sumityouhub/journalApp/service/UserDetailsServiceImplTests.java
@@ -0,0 +1,36 @@
+package net.sumityouhub.journalApp.service;
+
+import net.sumityouhub.journalApp.entity.User;
+import net.sumityouhub.journalApp.repository.UserRepository;
+import net.sumityouhub.journalApp.services.UserDetailsServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.ArrayList;
+import static org.mockito.Mockito.when;
+
+public class UserDetailsServiceImplTests {
+
+    @InjectMocks
+    private UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void loadUserByUsernameTest() {
+        when(userRepository.findByUserName(ArgumentMatchers.anyString())).thenReturn(User.builder().userName("ram").password("@#34hsi35hui4h35").roles(new ArrayList<>()).build());
+        UserDetails user = userDetailsServiceImpl.loadUserByUsername("ram");
+        Assertions.assertNotNull(user);
+    }
+}
